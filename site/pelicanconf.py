0c6d24608574e0e494debea8d06ee5f300ad0030
z <z@xnz.me>
2016-09-05 10:46:40 -0400

    put articles in their own directory and fix a bug in breadcrumb generation

--- site/pelicanconf.py
+++ site/pelicanconf.py
@@ -19,6 +19,10 @@ DIRECT_TEMPLATES = (('index', 'tags', 'categories', 'archives', 'search'))
 PLUGIN_PATHS = ["plugins", "/home/z/dev/pelican-plugins/"]
 PLUGINS = ["liquid_tags", "sitemap", "tipue_search"]
 
+ARTICLE_PATHS = ['articles']
+ARTICLE_SAVE_AS = 'articles/{date:%Y}/{slug}.html'
+ARTICLE_URL = 'articles/{date:%Y}/{slug}.html'
+
 TIPUE_SEARCH = True
 
 DEFAULT_DATE_FORMAT = '%b %d, %Y %I:%M %p'
@@ -32,11 +36,11 @@ TIMEZONE = 'America/Cancun'
 DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
+# FEED_ALL_ATOM = None
+# CATEGORY_FEED_ATOM = None
+# TRANSLATION_FEED_ATOM = None
+# AUTHOR_FEED_ATOM = None
+# AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('here', '#'),)
